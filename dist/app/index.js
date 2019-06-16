86c4ed212222050ecafcb6caf300d72d934c7236
SoraKumo <info@croud.jp>
2019-06-16 23:14:33 +0900

    パッケージの更新

--- dist/app/index.js
+++ dist/app/index.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const amf = require("active-module-framework");
 const path = require("path");
 new amf.Manager({
-    remotePath: '/',
-    execPath: '/',
-    indexPath: path.resolve(__dirname, '../template/index.html'),
-    rootPath: path.resolve(__dirname, '../public'),
-    cssPath: ['css'],
-    jsPath: ['js'],
-    localDBPath: path.resolve(__dirname, '../db/app.db'),
-    modulePath: path.resolve(__dirname, './modules'),
+    remotePath: "/",
+    execPath: "/",
+    indexPath: path.resolve(__dirname, "../template/index.html"),
+    rootPath: path.resolve(__dirname, "../public"),
+    cssPath: ["css"],
+    jsPath: ["js"],
+    localDBPath: path.resolve(__dirname, "../db/app.db"),
+    modulePath: path.resolve(__dirname, "./modules"),
     jsPriority: [],
     debug: true,
     listen: 8080 //受付ポート/UNIXドメインソケット
